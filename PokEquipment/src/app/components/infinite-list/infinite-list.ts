d01fa040ed9a4b40de47f23b986956f47744eba9
Victor Bechet <victorbechet5@gmail.com>
2026-02-06 11:33:59 +0100

    List display inline

--- PokEquipment/src/app/components/infinite-list/infinite-list.ts
+++ PokEquipment/src/app/components/infinite-list/infinite-list.ts
@@ -12,7 +12,7 @@ import { forkJoin, Observable, map } from 'rxjs';
   styleUrl: './infinite-list.css',
 })
 export class InfiniteList implements OnInit {
-  pokemonsIds: number[] = [1, 2, 3, 4, 5, 6, 7, 8];
+  pokemonsIds: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40];
   sprites$!: Observable<string[]>;
   
   constructor(private pokemonService: PokemonService) {}
