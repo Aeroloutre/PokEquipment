2333b1254a51539b2f498845d4b018613a206c62
Victor Bechet <victorbechet5@gmail.com>
2026-02-06 10:04:57 +0100

    List now display pokemon sprites correctly

--- DofEquipment/src/app/components/infinite-list/infinite-list.ts
+++ DofEquipment/src/app/components/infinite-list/infinite-list.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Header } from '../header/header';
+import { Loader } from '../loader/loader';
+import { AsyncPipe } from '@angular/common';
+import { PokemonService } from '../../services/pokemon.service';
+import { forkJoin, Observable, map } from 'rxjs';
 
 @Component({
   selector: 'app-infinite-list',
-  imports: [Header],
+  imports: [Header, Loader, AsyncPipe],
   templateUrl: './infinite-list.html',
   styleUrl: './infinite-list.css',
 })
-export class InfiniteList {
-
+export class InfiniteList implements OnInit {
+  pokemonsIds: number[] = [1, 2, 3, 4, 5, 6, 7, 8];
+  sprites$!: Observable<string[]>;
+  
+  constructor(private pokemonService: PokemonService) {}
+  
+  ngOnInit() {
+    const requests = this.pokemonsIds.map(id => this.pokemonService.getPokemonById(id));
+    
+    this.sprites$ = forkJoin(requests).pipe(
+      map((pokemons: any[]) => pokemons.map(p => p.sprites.front_default))
+    );
+  }
 }
---
+++ DofEquipment/src/app/services/pokemon.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PokemonService {
+  
+  constructor(private http: HttpClient) {}
+  
+  getPokemon() {
+    return this.http.get('https://pokeapi.co/api/v2/pokemon');
+  }
+  
+  getPokemonById(id: number) {
+    return this.http.get(`https://pokeapi.co/api/v2/pokemon/${id}`);
+  }
+}
