b5fa9b6d3827a7ec75b33479f2c7ffdbd8b90c81
Victor Bechet <victorbechet5@gmail.com>
2026-02-06 15:05:45 +0100

    switching button routerLink in header directly

--- PokEquipment/src/app/components/button/button.ts
+++ PokEquipment/src/app/components/button/button.ts
@@ -1,13 +1,10 @@
 import { Component, Input } from '@angular/core';
-import { RouterLink } from '@angular/router';
-
 @Component({
   selector: 'app-button',
-  imports: [RouterLink],
+  imports: [],
   templateUrl: './button.html',
   styleUrl: './button.css',
 })
 export class Button {
   @Input() title: string = 'Cliquez';
-  @Input() link: string = '';
 }
